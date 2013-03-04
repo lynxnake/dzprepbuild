06167f48520afbea8bf8838dbbef13000261e746
twm <twm@d789b566-a6c7-4212-b551-86ec34b0def4>
2013-03-04 17:25:16 +0000

    updated build script (from buildtools)

--- _BuildProject.cmd
+++ _BuildProject.cmd
@@ -3,22 +3,22 @@ REM - assumes that the parent directory is also the project name, extracts it
 REM - and calls msbuild with %project%.dproj
 
 setlocal
+set project=%1
 if not "%project%"=="" goto projgiven
 call :GetLastDir %0
 set project=%result%
-pushd %directory%
 
 :projgiven
 
-echo building project %project%.dproj using Delphi XE
+echo building project %project%.dproj using Delphi XE2
 
 call buildtools\delphiversions.cmd
 
+if not exist "%DelphiXE2Dir%\bin\rsvars.bat" goto nodelphi
 call "%DelphiXE2Dir%\bin\rsvars.bat"
 
 pushd src
-msbuild %project%.dproj | ..\buildtools\msbuildfilter
-popd
+msbuild %project%.dproj | ..\buildtools\msbuildfilter ..\errors.txt
 popd
 
 if errorlevel 1 goto error
@@ -28,10 +28,33 @@ pause
 :nopause
 goto :eof
 
+:nodelphi
+echo ************************************
+echo ****** Error building %project%
+echo ****** Delphi installation missing
+echo ************************************
+if "%MAILSend%"=="" goto nomail
+%MAILSend% -sub "Error building %project%" -M "Delphi installation missing"
+popd
+endlocal
+goto :eof
+
+:nomail
+popd
+endlocal
+pause
+goto :eof
+
 :error
 echo ************************************
-echo ***** Error building %project% *****
+echo ****** Error building %project%
 echo ************************************
+if "%MAILSend%"=="" goto nomail
+%MAILSend% -sub "Error building %project%" -M "Build Error" -attach errors.txt,text/plain,a
+endlocal
+goto :eof
+
+:nomail
 endlocal
 pause
 goto :eof
